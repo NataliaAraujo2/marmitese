3671c021591104eb8b34333431a6ffe82cb7080d
NataliaAraujo2 <nataliaanog@gmail.com>
2023-08-28 06:30:30 -0300

    Cadastra usuario

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,32 +1,63 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./Register.module.css";
 import Button from "../../components/Button/Button";
 import { FaBuilding, FaPen, FaUser } from "react-icons/fa";
+import { useAuthentication } from "../../hooks/useAuthentication";
 
 const Register = () => {
   const [displayName, setDisplayName] = useState("");
-  const [displayEmail, setDisplayEmail] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState("");
-  const [natural, setNatural] = useState(false);
+  const [natural, setNatural] = useState(true);
+  const displayNameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const confirmPasswordRef = useRef(null);
 
-  const handleSubmit = (e) => {
+  const { createUser, error: authError, loading } = useAuthentication();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-
+    setConfirmPassword("")
+    setDisplayName("")
+    setEmail("")
+    setPassword("")
     setError("");
 
     const user = {
       displayName,
-      displayEmail,
+      email,
       password,
     };
 
     if (password !== confirmPassword) {
-      setError("A senha e a confirmação precisam ser iguais!");
+      setError("As senhas precisam ser iguais!");
+      setPassword("");
+      setConfirmPassword("");
+      passwordRef.current.focus();
+      return;
     }
-  };
 
+    if (displayName === "") {
+      setError("Preencha todos os campos");
+      displayNameRef.current.focus();
+      return;
+    }
+
+    if (email === "") {
+      setError("Preencha todos os campos");
+      emailRef.current.focus();
+      return;
+    }
+    const res = await createUser(user);
+  };
+  useEffect(() => {
+    if (authError) {
+      setError(authError);
+    }
+  }, [authError]);
   return (
     <div className={styles.register}>
       <h1>CRIE SUA CONTA</h1>
@@ -36,7 +67,7 @@ const Register = () => {
         <ul>
           <li>
             <Button
-              action={() => setNatural(!natural)}
+              action={() => setNatural(true)}
               className={styles.natural}
               Text="Pessoa Física"
               Icon={FaUser}
@@ -45,7 +76,7 @@ const Register = () => {
           <li>
             <Button
               className={styles.legal}
-              action={() => setNatural(!natural)}
+              action={() => setNatural(false)}
               Text="Pessoa Jurídica"
               Icon={FaBuilding}
             />
@@ -60,7 +91,7 @@ const Register = () => {
               <input
                 type="text"
                 name="displayName"
-                required
+                ref={displayNameRef}
                 placeholder="Nome de Usuário"
                 value={displayName}
                 onChange={(e) => setDisplayName(e.target.value)}
@@ -70,11 +101,11 @@ const Register = () => {
               <span>Email:</span>
               <input
                 type="email"
-                name="displayEmail"
-                required
+                name="email"
+                ref={emailRef}
                 placeholder="Digite seu Email"
-                value={displayEmail}
-                onChange={(e) => setDisplayEmail(e.target.value)}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </label>
             <label>
@@ -82,10 +113,12 @@ const Register = () => {
               <input
                 type="password"
                 name="password"
+                ref={passwordRef}
                 required
                 placeholder="Insira sua Senha"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
+                autoComplete="off"
               />
             </label>
             <label>
@@ -93,20 +126,38 @@ const Register = () => {
               <input
                 type="password"
                 name="confirmPassword"
+                ref={confirmPasswordRef}
                 required
                 placeholder="Confirme sua senha"
                 value={confirmPassword}
                 onChange={(e) => setConfirmPassword(e.target.value)}
+                autoComplete="off"
               />
             </label>
-            <div className={styles.button}>
-              <ul>
-                <li>
-                  <Button action Text="Próximo" Icon={FaPen} />
-                </li>
-              </ul>
-            </div>
+            {!loading && (
+              <div className={styles.button}>
+                <ul>
+                  <li>
+                    <button>
+                      Cadastrar <FaPen />
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            )}
+            {loading && (
+              <div className={styles.button}>
+                <ul>
+                  <li>
+                    <button disabled>
+                      Aguarde... <FaPen />
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            )}
           </form>
+          {error && <p className="error">{error}</p>}
         </div>
       ) : (
         <div className={styles.legalform}>
@@ -116,7 +167,7 @@ const Register = () => {
               <input
                 type="text"
                 name="displayName"
-                required
+                ref={displayNameRef}
                 placeholder="Nome de Usuário"
                 value={displayName}
                 onChange={(e) => setDisplayName(e.target.value)}
@@ -126,11 +177,12 @@ const Register = () => {
               <span>Email:</span>
               <input
                 type="email"
-                name="displayEmail"
+                name="email"
+                ref={emailRef}
                 required
                 placeholder="Digite seu Email"
-                value={displayEmail}
-                onChange={(e) => setDisplayEmail(e.target.value)}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </label>
             <label>
@@ -138,10 +190,12 @@ const Register = () => {
               <input
                 type="password"
                 name="password"
+                ref={passwordRef}
                 required
                 placeholder="Insira sua Senha"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
+                autoComplete="off"
               />
             </label>
             <label>
@@ -149,25 +203,38 @@ const Register = () => {
               <input
                 type="password"
                 name="confirmPassword"
+                ref={confirmPasswordRef}
                 required
                 placeholder="Confirme sua senha"
                 value={confirmPassword}
                 onChange={(e) => setConfirmPassword(e.target.value)}
+                autoComplete="off"
               />
             </label>
-            <div className={styles.button}>
-              <ul>
-                <li>
-                  <Button
-                    className={styles.buttonc}
-                    action
-                    Text="Próximo"
-                    Icon={FaPen}
-                  />
-                </li>
-              </ul>
-            </div>
+            {!loading && (
+              <div className={styles.button}>
+                <ul>
+                  <li>
+                    <button>
+                      Cadastrar <FaPen />
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            )}
+            {loading && (
+              <div className={styles.button}>
+                <ul>
+                  <li>
+                    <button disabled>
+                      Aguarde... <FaPen />
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            )}
           </form>
+          {error && <p className="error">{error}</p>}
         </div>
       )}
     </div>
